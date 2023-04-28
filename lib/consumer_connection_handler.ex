87c1cc069caa8af1e88a2f1809a74a99d036fec8
markus-17 <marius.purici@isa.utm.md>
2023-04-28 10:23:36 +0300

    Initial logic for consumer commands

--- lib/consumer_connection_handler.ex
+++ lib/consumer_connection_handler.ex
@@ -5,11 +5,33 @@ defmodule ConsumerConnectionHandler do
     case :gen_tcp.recv(socket, 0) do
       {:ok, data} ->
         Logger.info("Consumer Data Received")
-        :gen_tcp.send(socket, "#{data |> String.trim}___")
+        handle_command(socket, data)
         serve(socket)
 
       {:error, :closed} ->
         Logger.info("Consumer Connection Closed")
+    end
+  end
+
+  defp handle_command(socket, string) do
+    string =
+      string
+      |> String.trim()
+      |> String.downcase()
+      |> String.split()
+
+    case string do
+      ["user", user_name] ->
+        :gen_tcp.send(socket, "\r\nUser was set to #{user_name}")
+
+      ["subscribe", topic] ->
+        :gen_tcp.send(socket, "\r\nSubscribed to topic #{topic}")
+
+      ["unsubscribe", topic] ->
+        :gen_tcp.send(socket, "\r\nUnsubscribed from topic #{topic}")
+
+      _ ->
+        :gen_tcp.send(socket, "\r\nCommand has the wrong format")
     end
   end
 end
