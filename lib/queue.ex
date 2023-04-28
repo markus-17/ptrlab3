aed049a9d8cfe453a69193d647417ac02d5b1acf
markus-17 <marius.purici@isa.utm.md>
2023-04-28 13:22:25 +0300

    Handle Calls to subscribe and get topics

--- lib/queue.ex
+++ lib/queue.ex
@@ -14,7 +14,26 @@ defmodule Queue do
 
   @impl true
   def init(user_name) do
-    {:ok, %{user_name: user_name}}
+    {:ok, %{user_name: user_name, topics: MapSet.new()}}
+  end
+
+  @impl true
+  def handle_call({:subscribe, topic}, _from, state) do
+    new_topics = state.topics |> MapSet.put(topic)
+    {:reply, :ok, state |> Map.put(:topics, new_topics)}
+  end
+
+  @impl true
+  def handle_call({:get_topics}, _from, state) do
+    {:reply, state.topics |> Enum.to_list(), state}
+  end
+
+  def subscribe(server, topic) do
+    :ok = GenServer.call(server, {:subscribe, topic})
+  end
+
+  def get_topics(server) do
+    GenServer.call(server, {:get_topics})
   end
 
   def get_name(user_name) do
