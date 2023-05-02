ea28a3e5ef19f166ab87594b7e89d7c8d3d566ee
markus-17 <marius.purici@isa.utm.md>
2023-05-02 19:48:27 +0300

    Add publish logic for producers

--- lib/producer_connection_handler.ex
+++ lib/producer_connection_handler.ex
@@ -5,11 +5,32 @@ defmodule ProducerConnectionHandler do
     case :gen_tcp.recv(socket, 0) do
       {:ok, data} ->
         Logger.info("Producer Data Received")
-        :gen_tcp.send(socket, "#{data |> String.trim}___")
+        handle_message(data)
         serve(socket)
 
       {:error, :closed} ->
         Logger.info("Producer Connection Closed")
     end
+  end
+
+  defp handle_message(data) do
+    [topic, message] =
+      data
+      |> String.trim()
+      |> String.split("@")
+
+    topic =
+      topic
+      |> String.trim()
+      |> String.downcase()
+
+    message =
+      message
+      |> String.trim()
+
+    Supervisor.which_children(QueueSupervisor)
+    |> Enum.map(fn {_, pid, _, _} ->
+      Queue.send_message(pid, topic, message)
+    end)
   end
 end
